a6d60abbc1dd037e152715bc8077cb6149f56ee2
Yemi Tula <yemi@tulabyte.net>
2020-04-14 20:44:54 +0100

    correction 11

--- src/estimator.js
+++ src/estimator.js
@@ -20,21 +20,21 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
   // get factor by dividing number of days by 3
-  const factor = Math.round(days / 3);
+  const factor = Math.ceil(days / 3);
   // compute infectionsByRequestedTime using currentlyInfected and factor
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
 
   // CHALLENGE 2
   // compute severeCasesByRequestedTime
-  impact.severeCasesByRequestedTime = Math.round(
+  impact.severeCasesByRequestedTime = Math.ceil(
     0.15 * impact.infectionsByRequestedTime
   );
-  severeImpact.severeCasesByRequestedTime = Math.round(
+  severeImpact.severeCasesByRequestedTime = Math.ceil(
     0.15 * severeImpact.infectionsByRequestedTime
   );
   // compute number of available hospital beds
-  const bedsAvailable = Math.round(0.35 * data.totalHospitalBeds);
+  const bedsAvailable = Math.ceil(0.35 * data.totalHospitalBeds);
   // compute number of available beds for COVID-19
   impact.hospitalBedsByRequestedTime = bedsAvailable - impact.severeCasesByRequestedTime;
   severeImpact.hospitalBedsByRequestedTime = bedsAvailable
@@ -42,17 +42,17 @@ const covid19ImpactEstimator = (data) => {
 
   // CHALLENGE 3
   // compute casesForICUByRequestedTime
-  impact.casesForICUByRequestedTime = Math.round(
+  impact.casesForICUByRequestedTime = Math.ceil(
     0.05 * impact.infectionsByRequestedTime
   );
-  severeImpact.casesForICUByRequestedTime = Math.round(
+  severeImpact.casesForICUByRequestedTime = Math.ceil(
     0.05 * severeImpact.infectionsByRequestedTime
   );
   // compute casesForVentilatorsByRequestedTime
-  impact.casesForVentilatorsByRequestedTime = Math.round(
+  impact.casesForVentilatorsByRequestedTime = Math.ceil(
     0.02 * impact.infectionsByRequestedTime
   );
-  severeImpact.casesForVentilatorsByRequestedTime = Math.round(
+  severeImpact.casesForVentilatorsByRequestedTime = Math.ceil(
     0.02 * severeImpact.infectionsByRequestedTime
   );
   // compute dollarsInFlight
