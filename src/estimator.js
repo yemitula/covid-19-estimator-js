af0b160b22d2f738e21f7e35970d3012f6fae959
Yemi Tula <yemi@tulabyte.net>
2020-04-14 19:42:05 +0100

    corrections 3

--- src/estimator.js
+++ src/estimator.js
@@ -4,14 +4,15 @@ const covid19ImpactEstimator = (data) => {
 
   // compute number of days
   let days = data.timeToElapse;
-  switch (periodType) {
+  switch (data.periodType) {
     case 'weeks':
       days = data.timeToElapse * 7;
       break;
-    
     case 'months':
       days = data.timeToElapse * 30;
       break;
+    default:
+      days = data.timeToElapse;
   }
 
   // CHALLENGE 1
@@ -21,45 +22,29 @@ const covid19ImpactEstimator = (data) => {
   // get factor by dividing number of days by 3
   const factor = days / 3;
   // compute infectionsByRequestedTime using currentlyInfected and factor
-  impact.infectionsByRequestedTime =
-    impact.currentlyInfected * Math.pow(2, factor);
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * Math.pow(2, factor);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, factor);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2**factor);
 
   // CHALLENGE 2
   // compute severeCasesByRequestedTime
   impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
-  severeImpact.severeCasesByRequestedTime =
-    0.15 * severeImpact.infectionsByRequestedTime;
+  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
   // compute number of available hospital beds
   const bedsAvailable = 0.35 * data.totalHospitalBeds;
   // compute number of available beds for COVID-19
-  impact.hospitalBedsByRequestedTime =
-    bedsAvailable - impact.severeCasesByRequestedTime;
-  severeImpact.hospitalBedsByRequestedTime =
-    bedsAvailable - severeImpact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = bedsAvailable - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = bedsAvailable - severeImpact.severeCasesByRequestedTime;
 
   // CHALLENGE 3
   // compute casesForICUByRequestedTime
   impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
-  severeImpact.casesForICUByRequestedTime =
-    0.05 * severeImpact.infectionsByRequestedTime;
+  severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
   // compute casesForVentilatorsByRequestedTime
-  impact.casesForVentilatorsByRequestedTime =
-    0.02 * impact.infectionsByRequestedTime;
-  severeImpact.casesForVentilatorsByRequestedTime =
-    0.02 * severeImpact.infectionsByRequestedTime;
+  impact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
+  severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
   // compute dollarsInFlight
-  impact.dollarsInFlight =
-    impact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD *
-    days;
-  severeImpact.dollarsInFlight =
-    severeImpact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD *
-    days;
+  impact.dollarsInFlight = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * days;
+  severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * days;
 
   return {
     data: data,
